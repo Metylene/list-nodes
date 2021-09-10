a3aec833786ac75fdf95c9b5eead730c2e6db94f
Metylene <aymerickpdev@gmail.com>
2021-09-10 14:34:08 +0200

    Add color by faction and basic sorting

--- dist/js/app.js
+++ dist/js/app.js
@@ -18,6 +18,10 @@ $ = function (first, second = null) {
 // Oil Well         (41)
 // Rocket Site      (37)
 
+let regionsName = []; //https://war-service-live.foxholeservices.com/api/worldconquest/maps
+regionsDynamicData = []; // [{regionName : https://war-service-live.foxholeservices.com/api/worldconquest/maps/' + regionName + '/dynamic/public}, ...]
+regionsWithWardenStorage = []; // [{regionName : {storage : [cityName1, cityName2, ...]}, ...] 
+
 const mapIconsIndexes = ['20', '21', '23', '38', '40', '32', '41', '37'];
 const nodesTableBody = $("#nodesTables tbody");
 fetch('https://war-service-live.foxholeservices.com/api/worldconquest/maps')
@@ -30,11 +34,11 @@ fetch('https://war-service-live.foxholeservices.com/api/worldconquest/maps')
             if (hex.startsWith("Home")) { continue; }
 
             const hexRowElt = document.createElement("tr");
-            const thCell = document.createElement("th");
+            const thCell = document.createElement("td");
             thCell.innerText = hex.replace("Hex", "").replace(/([A-Z])/g, ' $1').trim();
             thCell.scope = "row";
             hexRowElt.appendChild(thCell);
-            for (let j = 0; j < mapIconsIndexes.length; j++) {
+            for (let j = 0; j < mapIconsIndexes.length + 1; j++) {
                 const tdCell = document.createElement("td");
                 tdCell.innerText = 0;
                 hexRowElt.appendChild(tdCell);
@@ -48,16 +52,31 @@ fetch('https://war-service-live.foxholeservices.com/api/worldconquest/maps')
                 .then(function (hexData) {
                     const mapItems = hexData['mapItems'];
                     if (mapItems == null) { console.log("no mapItems ? ", hexData); return; }
+                    let containWardenItem = false;
+                    let containColiItem = false;
                     for (key in mapItems) {
                         const item = mapItems[key];
                         const iconType = item['iconType'] + "";
+                        if (item['teamId'] === 'WARDENS') containWardenItem = true;
+                        if (item['teamId'] === 'COLONIALS') containColiItem = true;
                         if (mapIconsIndexes.includes(iconType)) {
                             let itemCell = hexRowElt.children[mapIconsIndexes.indexOf(iconType) + 1];
                             itemCell.innerText = parseInt(itemCell.innerText) + 1;
                         }
 
                     }
-
+                    if (containWardenItem && containColiItem) {
+                        let itemCell = hexRowElt.children[hexRowElt.children.length - 1];
+                        itemCell.innerText = "Mixte";
+                    } else if (containWardenItem) {
+                        let itemCell = hexRowElt.children[hexRowElt.children.length - 1];
+                        itemCell.innerText = "Warden";
+                        hexRowElt.classList.add('warden');
+                    } else if (containColiItem) {
+                        let itemCell = hexRowElt.children[hexRowElt.children.length - 1];
+                        itemCell.innerText = "Colonial";
+                        hexRowElt.classList.add('colonial');
+                    }
                 })
                 .catch(function (error) {
                     console.log(error.message);
@@ -65,3 +84,32 @@ fetch('https://war-service-live.foxholeservices.com/api/worldconquest/maps')
 
         }
     });
+
+function sortTableByColumn(thCellElt) {
+    const table = document.getElementById('nodesTables');
+    const columnId = Array.prototype.indexOf.call(thCellElt.parentNode.children, thCellElt);
+    console.log("columnId : ", columnId);
+    let i;
+    let switching = true;
+
+    while (switching) {
+        switching = false;
+        let rows = table.rows;
+        console.log(rows);
+        let shouldSwitch = false;
+        // we don't take first row because it's the th header
+        for (i = 1; i < (rows.length - 1); i++) {
+            let x = rows[i].getElementsByTagName('td')[columnId];
+            let y = rows[i + 1].getElementsByTagName('td')[columnId];
+
+            if(x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()){
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+        }
+    }
+}
